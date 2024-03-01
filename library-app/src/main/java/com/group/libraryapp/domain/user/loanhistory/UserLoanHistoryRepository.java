c0ae6a4771f8be5c2d83c4b9f9607687ba6c1d96
HyeJu Han <metaannotation@gmail.com>
2024-03-01 11:24:41 +0900

    Delete library-app directory

--- library-app/src/main/java/com/group/libraryapp/domain/user/loanhistory/UserLoanHistoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.group.libraryapp.domain.user.loanhistory;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserLoanHistoryRepository extends JpaRepository<UserLoanHistory, Long> {
-
-    boolean existsByBookNameAndIsReturn(String name, boolean isReturn);
-}
