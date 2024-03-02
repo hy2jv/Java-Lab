acf2d3ff7d1a116fee6f3e4113c005110cc532df
hanhyeju <hy2jv@naver.com>
2024-03-02 13:04:50 +0900

    section 8 Environment Property

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,10 +1,24 @@
 package tobyspring.helloboot;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import tobyspring.config.MySpringBootApplication;
 
 @MySpringBootApplication
 public class HellobootApplication {
+
+	// Properties 파일보다 환경 변수를 우선시하여 property로 읽힌다.
+	// 추가적으로 환경 변수보다는 시스템 property가 더 우선된다.
+	@Bean
+	ApplicationRunner applicationRunner(Environment env) {
+		return args -> {
+			String name = env.getProperty("my.name");
+			System.out.println("my.name: " + name);
+		};
+    }
 
     public static void main(String[] args) {
 		SpringApplication.run(HellobootApplication.class, args);
