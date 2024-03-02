58c74fd37e46444d6368be3602697ef678e936ed
hanhyeju <hy2jv@naver.com>
2024-03-03 00:27:08 +0900

    section 9 JdbcTemplate practice

--- src/main/java/tobyspring/config/autoconfig/DataSourceConfig.java
+++ src/main/java/tobyspring/config/autoconfig/DataSourceConfig.java
@@ -2,8 +2,12 @@ package tobyspring.config.autoconfig;
 
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnSingleCandidate;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.jdbc.support.JdbcTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import tobyspring.config.ConditionalMyOnClass;
 import tobyspring.config.EnableMyConfigurationProperties;
 import tobyspring.config.MyAutoConfiguration;
@@ -14,6 +18,7 @@ import java.sql.Driver;
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.springframework.jdbc.core.JdbcOperations")
 @EnableMyConfigurationProperties(MyDataSourceProperties.class)
+@EnableTransactionManagement
 public class DataSourceConfig {
     @Bean
     @ConditionalMyOnClass("com.zaxxer.hikari.HikariDataSource")
@@ -40,5 +45,23 @@ public class DataSourceConfig {
         dataSource.setPassword(properties.getPassword());
 
         return dataSource;
+    }
+
+    @Bean
+    @ConditionalOnSingleCandidate(DataSource.class)
+    /* 빈 메소드가 실행될 때 스프링 컨테이너의 빈 구성 정보에 DataSource 타입의 빈이 딱 한 개만 등록되어 있다면
+       그때 이 하나의 데이터 소스를 가져와서 사용하겠다는 것 */
+    @ConditionalOnMissingBean
+    JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+
+    @Bean
+    @ConditionalOnSingleCandidate(DataSource.class)
+    /* 빈 메소드가 실행될 때 스프링 컨테이너의 빈 구성 정보에 DataSource 타입의 빈이 딱 한 개만 등록되어 있다면
+       그때 이 하나의 데이터 소스를 가져와서 사용하겠다는 것 */
+    @ConditionalOnMissingBean
+    JdbcTransactionManager jdbcTransactionManager(DataSource dataSource) {
+        return new JdbcTransactionManager(dataSource);
     }
 }
