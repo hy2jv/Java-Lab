1b5ba624af0b62a40b9709586e94a06b13d2e594
hanhyeju <hy2jv@naver.com>
2024-03-02 13:46:47 +0900

    section 8 setting  contextPath & port

--- src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
@@ -11,13 +11,19 @@ import tobyspring.config.MyAutoConfiguration;
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
-    @Value("${contextPath}")
+    @Value("${contextPath: }") // default => context-path none(" ")
     String contextPath;
+
+    @Value("${port:8080}") // default 8080
+    int port;
+
     @Bean("tomcatWebServerFactory")
     @ConditionalOnMissingBean // 같은 타입의 빈이 있는지(사용자가 직접 등록한) 체크해보고 없다면, 이걸 빈으로 등록
     public ServletWebServerFactory servletWebServerFactory() {
         TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
         factory.setContextPath(this.contextPath); // context-path를 지정하면 모든 서블릿의 맵핑 앞에 context-path가 추가됨
+        factory.setPort(port);
+
         return factory;
     }
 
