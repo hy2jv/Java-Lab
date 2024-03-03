3cc22ab28bf383fff9ccce83ddacd2d1598ffc19
hanhyeju <hy2jv@naver.com>
2024-03-03 13:13:56 +0900

    section 9 HelloService using Repository

--- src/test/java/tobyspring/helloboot/HelloRepositoryTest.java
+++ src/test/java/tobyspring/helloboot/HelloRepositoryTest.java
@@ -5,18 +5,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @HellobootTest
 public class HelloRepositoryTest {
     @Autowired JdbcTemplate jdbcTemplate;
     @Autowired HelloRepository helloRepository;
-
-    @BeforeEach
-    void init() {
-        jdbcTemplate.execute("create table if not exists hello(name varchar(50) primary key, count int)");
-    }
 
     @Test
     void findHelloFailed() {
